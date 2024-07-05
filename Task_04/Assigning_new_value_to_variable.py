70878bb30fe87f94f9726e082be4b5ba505fbc27
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-05 16:31:22 +0500

    File is added to task-04
    A file assigning new value to variable

---
+++ Task_04/Assigning_new_value_to_variable.py
@@ -0,0 +1,8 @@
+# This program print the new assigned value 
+# to a variable
+
+msg = "Welcome to the world of Data Sceince!"
+print(msg)
+
+msg = "The world is waiting for you!"
+print(msg)
---
+++ Task_04/Printing_Simple_Message.py
@@ -0,0 +1,4 @@
+# This program simply prints a message
+
+msg = "Welcome to the world of Data Science!"
+print(msg)
