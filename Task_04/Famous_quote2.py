37895a9b049a80f4b2f5edaf073b31cb95776eac
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-08 09:26:21 +0500

    Famous person 2 file is added

---
+++ Task_04/Famous_quote2.py
@@ -0,0 +1,6 @@
+# This program prints famous_person name stored in 
+# a vairable and a message stored in a variable
+
+famous_person = "Hazrat Ali(RA)"
+message = "Calmness is a sign of intelligence."
+print(f'{famous_person} Once said,"{message}"')
