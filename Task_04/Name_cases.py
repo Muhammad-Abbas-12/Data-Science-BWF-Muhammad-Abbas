402227f2aad02ec3cdcded7fcab855f4579ed138
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-08 08:21:25 +0500

    Names cases is added

---
+++ Task_04/Name_cases.py
@@ -0,0 +1,7 @@
+# This programe print the person's name 
+# in upper, lowe, and title case
+
+name = "shahzad"
+print("The name is in upper case ", name.upper())  # printing upper case 
+print("The name is in lower case ", name.lower())  # printing lower case 
+print("The name is in title case ", name.title())  # printing title case 
