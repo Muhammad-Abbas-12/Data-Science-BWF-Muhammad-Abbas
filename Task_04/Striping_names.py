5d086f2eb9195dba741bcfbd60bb56588f544646
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-09 15:54:01 +0500

    Striping names file is added

---
+++ Task_04/Striping_names.py
@@ -0,0 +1,9 @@
+# This program shows how to strip white space 
+# from left, right and both.
+
+person_name = "   Zulfiqar Ahmad     "
+print(person_name)
+
+print(person_name.lstrip())     # left side strip
+print(person_name.rstrip())     # right side strip
+print(person_name.strip())      # both side strip
