c9f9b62f9cfcc47564e4a2cbd485f7364a925f1d
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-09 16:42:16 +0500

    Friends Greeting file is added

---
+++ Task_05/Greeting.py
@@ -0,0 +1,7 @@
+# The program prints each friends 
+# names in a list with a similer message
+
+friends_names = ["Adil", "Ishfaq", "Zakir", "Khizar"]
+
+for i in range(len(friends_names)):
+    print(f"I miss you {friends_names[i]}, when we will meet.")
