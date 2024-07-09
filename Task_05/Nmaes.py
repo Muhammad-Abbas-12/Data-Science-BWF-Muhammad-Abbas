02b69a987064a428817c3f8ebd843ae0c6a6c3e1
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-09 16:35:57 +0500

    Names file is added

--- Task_05/Nmaes.py
+++ Task_05/Nmaes.py
@@ -1,3 +1,7 @@
 # This program will show you how to make and
 # how to access elements of a list one by one
 
+friends_names = ["Adil", "Ishfaq", "Zakir", "Khizar"]
+
+for i in range(len(friends_names)):
+    print(friends_names[i])
