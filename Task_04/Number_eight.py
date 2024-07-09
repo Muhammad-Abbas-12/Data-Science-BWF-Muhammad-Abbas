ec83694dca0db7b555b936376a866af3cfe39270
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-09 16:07:37 +0500

    Number eight file is added

---
+++ Task_04/Number_eight.py
@@ -0,0 +1,8 @@
+# This program result in number 8
+# when performing addtion, substraction,
+# multiplication and division
+
+print(3+5)  # addition
+print(12-4) # substraction
+print(2*4)  # multiplication
+print(64/8) # division
