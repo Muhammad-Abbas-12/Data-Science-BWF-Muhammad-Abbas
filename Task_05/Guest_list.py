710aa314bfea646104e3d6d0a317cf6bf923be3b
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-10 12:31:39 +0500

    Guest LIst file is added

---
+++ Task_05/Guest_list.py
@@ -0,0 +1,7 @@
+# The program prints an invitation to 
+# to each person seperatly
+
+guest_list = ['Ali','Hasan','Imad','Waseem']
+
+for i in range(len(guest_list)):
+    print(f"Deer {guest_list[i]}!, The honor of your presence is requested for dinner at eight thirty in Meshbell's Hall.")
