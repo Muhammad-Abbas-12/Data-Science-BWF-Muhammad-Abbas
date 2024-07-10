72dc2dcaf018958d0290e432216b0b2e1c7fd491
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-10 12:07:28 +0500

    own list file is added

---
+++ Task_05/OwnLlist.py
@@ -0,0 +1,6 @@
+# The program print a series of staments
+# with each element of a list
+
+my_cars =['Maclaren_720s', 'Lambergini', 'Ferrari', 'Mastang','Mercedes']
+for i in range(len(my_cars)):
+    print(f"I would like to drive {my_cars[i]}")
