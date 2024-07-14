f049cf0888fa0f1ab2de2dbc3dc15451659b5b8a
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-14 14:47:24 +0500

    Dinner guest file is added

---
+++ Task_05/Dinner_guests.py
@@ -0,0 +1,25 @@
+# The program shows how to print lenth of a list
+
+guest_list = ['Ali','Hasan','Imad','Waseem']
+print("Number of guests invited")
+print(len(guest_list))
+
+for i in range(len(guest_list)):
+    print(f"Deer {guest_list[i]}!, The honor of your presence is requested for dinner at eight thirty in Meshbell's Hall.")
+
+print("The person's who didn't make it for a dinner are: 'Ali' and 'Imad'.")
+
+guest_list[0] = 'Abbas'
+guest_list[2] = 'Nasir'
+
+for i in range(len(guest_list)):
+    print(f"Deer! {guest_list[i]}!, Join us for a night of culinary delights and laughter at our dinner party")
+
+guest_list.insert(0,"wasem")  # Name waseem is added at the begaining of a list
+guest_list.insert(2, "Shadab") # Name shadab is added at the middle of a list
+guest_list.append('Salman')
+
+print("Number of guests invited after adding more people")
+print(len(guest_list))
+for i in range(len(guest_list)):
+        print(f"Deer {guest_list[i]}, We would be honored if you could join us for a delightful dinner party.")
