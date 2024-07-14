d1e1021033956607ab15596c62c5b4f33e88f419
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-14 14:54:22 +0500

    File name changed

---
+++ Task_05/Every_function.py
@@ -0,0 +1,7 @@
+# The show all the functions in chapter 03 applied to a list
+
+cities = ['Islamabad','Peshawar','Faisalabad','Karachi','Lahore','Jhang']
+print('The orignal list:')
+print(cities)
+print(f'Lenth of orignal list \n{len(cities)}')
+
