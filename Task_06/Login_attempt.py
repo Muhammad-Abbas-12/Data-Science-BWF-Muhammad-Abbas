64e9e7399fedd88ba201e8dd6b0d908c60ca9ea8
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-07-18 20:48:49 +0500

    login attempt file is added

--- Task_06/Login_attempt.py
+++ Task_06/Login_attempt.py
@@ -3,21 +3,38 @@
 
 class User():
     # initial method
-    def __init__(self, first_name, last_name, age, sex):
+    def __init__(self, first_name, last_name, age, sex, login_attempts):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.sex = sex
+        self.login_attempts = login_attempts 
 
     # defining class method
     def describe_user(self):
         print(self.first_name.title() +' ' +  self.last_name.title())
         print('Age: '+ str(self.age))
-        print('Sex: ' + self.sex)
+        print('Sex: ' + self.sex.title())
 
     def greet_user(self):
         print('Your are welcome here!')
         print('.'*30)
 
-    def login_attempt(self):
+    def increment_login_attempt(self):
+        # increments logins attempts
         
+        self.login_attempts = self.login_attempts + 1
+        print('Number of login attempts: ',self.login_attempts)
+       
+
+    def reset_login_attempt(self):
+        # reseting value to zero
+        self.login_attempts = 0
+        print(f"NUmber of login attempts are: {self.login_attempts}")
+
+# class instance
+user = User("ali","khan",33,"male",0)
+user.increment_login_attempt()
+user.increment_login_attempt()
+user.increment_login_attempt()
+user.reset_login_attempt()
