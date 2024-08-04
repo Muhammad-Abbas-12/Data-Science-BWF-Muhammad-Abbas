222b16d39e89dbfbb35630021fa452d49b3edcf5
Muhammad-Abbas-12 <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-08-04 18:48:41 +0500

    Dice file is added and ordered dic rewrite is added

---
+++ Task_06/Privileges_Admin_class_module.py
@@ -0,0 +1,20 @@
+# class Admin and Privileges module
+
+from Task_06.User_class_module import User
+
+class Admin(User):
+    
+    def __init__(self):
+     self.privileges = ['can add post','can delete post','can be user']
+
+    def show_privileges(self):
+        print('Priviliges are: ',self.privileges)
+
+
+class Privileges():
+
+    def __init__(self):
+        self.privileges = ['can add users','can remove users','can be a user']
+
+    def show_privileges(self):
+        print('Priviliges are: ',self.privileges)
