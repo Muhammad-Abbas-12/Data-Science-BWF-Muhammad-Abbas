54c035d3ba1e8b2949287b1f43c1000f8512ff3c
Muhammad Abbas <79800266+Muhammad-Abbas-12@users.noreply.github.com>
2024-08-07 03:03:41 +0000

    multi dimentional arrays file is added

---
+++ Task_08/Multi_dimentional_arrays.py
@@ -0,0 +1,20 @@
+# multi dimentional array
+
+import numpy as np
+
+# one dimentional array
+
+a1 = np.array([1,2,3,4,5])
+a1
+
+# two dimentional array
+
+a2 = np.array(([[1,2,3],[4,5,6],[7,8,9]],[[9,8,7],[6,5,4],[3,2,1]]))
+a2
+a2.shape
+
+# 3 dimentional array
+
+a3 = np.array([[[[1,1,1],[2,2,2],[3,3,3,]], [[4,4,4],[5,5,5],[6,6,6]]]])
+a3
+a3.shape 
